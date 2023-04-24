2c96ff79a1965726cba0889e0612f11c4128b178
Mitch Wheat <mitch.wheat@gmail.com>
2023-04-24 13:18:03 +0800

    Update Date Dimension.sql

--- Date Dimension.sql
+++ Date Dimension.sql
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2021 Mitch Wheat
+Copyright (c) 2023 Mitch Wheat
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
